79787016388bf2f453ad826f1c8136d5c395fa56
James Rebello <Jikrebello.za@gmail.com>
2021-09-30 10:00:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator_For_Devloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
